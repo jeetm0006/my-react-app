3d44ccd2e464f359614abc746aad907f7082c9cb
jeet <jeetm0006@gmail.com>
2020-08-21 22:41:09 +0530

    learning set effect

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import classes from "./Cockpit.css";
 
 const Cockpit = (props) => {
+
+  useEffect(() => {
+    console.log("useEffect is called");
+  },[props.persons]);
+
   let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Red;
